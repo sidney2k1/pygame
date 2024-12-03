63d88eea06135c08a7dafaf27cae5d181c8a6c2d
sidney2k1 <sidsingla02@gmail.com>
2024-12-03 17:51:10 +0530

    project

--- project.py
+++ project.py
@@ -1,20 +1,20 @@
 import pygame
 pygame.init()
-white=(255,255,255)
+grey=(170,170,170)
 clock=pygame.time.Clock()
 displaysurface=pygame.display.set_mode((500,500))
-pygame.display.set_caption("image")
-image=pygame.image.load("download (3).jpeg")
+pygame.display.set_caption("My first game display")
+image=pygame.image.load("download.jpeg")
 defaultsizeimage=(200,200)
 image=pygame.transform.scale(image,defaultsizeimage)
 defaultposition=(1,1)
-image2=pygame.image.load("download.jpeg")
+image2=pygame.image.load("download (3).jpeg")
 defaultsizeimage2=(200,300)
 image2=pygame.transform.scale(image2,defaultsizeimage2)
 defaultposition2=(1,200)
 
 while True:
-    displaysurface.fill(white)
+    displaysurface.fill(grey)
     displaysurface.blit(image,defaultposition)
     displaysurface.blit(image2,defaultposition2)
     for event in pygame.event.get():
